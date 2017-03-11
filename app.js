0796930556f39e9a3b22ecb018c17f55ba8419ef
Taniesha Robinson <taniesharobinson@dhcp-10-105-139-145.wireless.northwestern.private>
2017-03-11 14:54:17 -0600

    gif begins on click

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 $('#submit-bid').on('click', function(event){
 	event.preventDefault();
 	console.log("let's create a row");
-	$('tbody').append('tr');
+	$('tbody').append('<tr>');
+
+	$('tbody:last-child').append(
+		
+	);
 });
